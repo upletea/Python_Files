e79cfe9af7fbcf864c1cbac921d26be7964e012a
upletea <aapti.dileepsharma603@education.nsw.gov.au>
2025-06-20 12:00:38 +1000

    Update challenge_e1.py

--- challenge_e1.py
+++ challenge_e1.py
@@ -1,5 +1,5 @@
-num1 = int(input("Enter your first number:"))  #user inputs their first number as an integer      
-num2 = int(input("Enter your second number:"))  #user inputs second number as an integer
+num1 = int(input("Enter your first number:"))  #prompts user to input their first number as an integer      
+num2 = int(input("Enter your second number:"))  #prompts user to input second number as an integer
 #ensure it is typed exactly (adding, subtracting) 
 operation = ["adding", "subtracting"] #creates a list of operations that comes into play later on in the code
  
@@ -7,16 +7,20 @@ for i in operation:
     print(i," --") #prints the operation options for the user to choose from
 operation_input = input("What operation would you like to use: adding or subtracting?")#prompts user to input if they would like to add or subtract
 
-def calculator(operation_input, num1, num2):
-    if operation_input == "adding":
-       return num1 + num2
-    elif operation_input == "subtracting":
-       return num1 - num2
+def calculator(operation_input, num1, num2): #makes a function and defines it
+    if operation_input == "adding": # if the input is adding...
+       return num1 + num2 #the number is returned as a combination of both numbers
+    elif operation_input == "subtracting": #if the input is subtraction...
+       return num1 - num2 #its returned as the product of that
     else:
-        print("Error, operation is unsupported. No answer can be given.")
+        print("Error, operation is unsupported. No answer can be given.") #if neither adding nor subtracting is chosen, then an error message is shown
     
-result = calculator(operation_input, num1, num2,)
-print("Your answer is", result)
+result = calculator(operation_input, num1, num2,) #calls the calculator function with user input and stores the result
+print("Your answer is", result) #displays the final result
+
+
+
+
 
 
 
