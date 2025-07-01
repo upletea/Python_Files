8b7a608da600f4837e82c7d8249bb5f246aa77f7
upletea <aapti.dileepsharma603@education.nsw.gov.au>
2025-07-01 12:50:06 +1000

    yayayay

--- challenge_f.py
+++ challenge_f.py
@@ -1,3 +1,4 @@
+# declare the dictionaries
 student_profile = {}
 enrolled_input = {}
 
@@ -6,20 +7,28 @@ student_profile["name"] = input("Enter the student's name: ")
 student_profile["year level"] = int(input("Enter the student year level (7, 8, 9, 10 ,11 or 12): "))
 student_profile["grade"] = input("Enter the student grade (A, B, C, D, E or F): ")
 student_profile["age"] = int(input("Enter the student's age: "))
-student_profile["enrolled"] = input("Is the student enrolled? (yes or no?) : ")
+student_profile["enrolled"] = input("Is the student enrolled? (Yes or No?) : ")
+
+if student_profile["enrolled"] == "Yes":
+    student_profile["enrolled"] = True
+else:
+    student_profile["enrolled"] = False
+
 
 # Displays the profile
-print("\Student Profile")
+print("~Student Profile~")
 print(f"Name: {student_profile['name']}")
 print(f"Year Level: {student_profile['year level']}")
 print(f"Grade: {student_profile['grade']}")
 print(f"Age: {student_profile['age']}")
-print(f"Enrolled:{student_profile['enrolled']}")
+print(f"Enrolled: {student_profile['enrolled']}")
 
-if enrolled_input == "yes":
+if student_profile["enrolled"] == True:
     print("Student is currently enrolled.")
 else:
-    print("Student is not currently enrolled.")
+   print("Student is not currently enrolled.")
+
+
 
 
 
