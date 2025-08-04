1a671005f4d6b26a35d9dcb106439aec2f02a57a
upletea <aapti.dileepsharma603@education.nsw.gov.au>
2025-08-05 09:08:55 +1000

    ccccccccccccccccccccccccccccccccccccccccc

--- assessment/functions.py
+++ assessment/functions.py
@@ -1,7 +1,17 @@
 import os
+import colorama
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def exit_game():
+    os._exit(1)
+
+def restart_level():
+   global player, score, exit_game
+   player.reset_position()
+   score = 0
+   exit_game = False
 
 from colorama import init, Fore, Style
 init(autoreset=True)  # Resets colour after each print
