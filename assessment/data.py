396a51395daf37886f0c1abdadfb4e3c100c864d
upletea <aapti.dileepsharma603@education.nsw.gov.au>
2025-08-05 09:10:17 +1000

    cccccccccccccccccccccccccccccccccc

--- assessment/data.py
+++ assessment/data.py
@@ -1,2 +1,9 @@
-rooms=['intro','drowning']
-room_text=['welcome to the game, you are in a dark corner and see a wet cat','you slip into the water and start to choke']
+rooms=['The Room of Reckoning','Waters of Undoing','Sanctum of Storms','The Ember Vault' , 'The Oasis Chamber','The Glacial Depths','Center of the Elements']
+rooms_text=['welcome to the game, you are in a dark corner and see a wet cat','you slip into the water and start to choke','', ]
+
+game_map =                   
+      [4]          [5]
+          
+[3]         [0]         [6]
+                                
+    [2]           [1]
