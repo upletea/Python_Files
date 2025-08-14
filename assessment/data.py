778a621d764886d3f9189ee5b5003de5c384bd8a
upletea <aapti.dileepsharma603@education.nsw.gov.au>
2025-08-15 09:13:01 +1000

    e

--- assessment/data.py
+++ assessment/data.py
@@ -1,21 +1,22 @@
 import random
 
-rooms=['The Room of Reckoning💡','Waters of Undoing🌊','Sanctum of Storms⛈️','The Ember Vault🔥' , 'The Oasis Chamber🌵','The Glacial Depths🧊 ','Center of the Elements']
-rooms_text=['You wake up to a deep groaning sound, head pounding and heart racing. "Where am I?" you wonder.  ','you slip into the water and start to choke','', ]
+rooms=['The Room of Reckoning💡','Waters of Undoing🌊','Sanctum of Storms⛈️','The Ember Vault🔥' , 'The Oasis Chamber🌵','Center of the Elements']
+rooms_text=['You wake up to a deep groaning sound, head pounding and heart racing. "Where am I?" you wonder. Frantically blinking, you attempt to reajust your eyes to the gloomy environment. Looking around, you see a dim flickering light, walls filled with rustic doors and a large table right in the centre. ','You awake, disoriented once again. Shaking your head vigorsly, you rise to your feet. A quiet crackling sound startles you. ','lalala', ]
 
 
-#game_map =
 
+player = ("name")
 player_health = 100 
-enemy_damage = random.choices(5, 0, 10, 15, 20, 40)
-enemy_health = random.choices(50, 100, 75, 150, 125, 25)
+enemy_damage = random.choice([5, 10, 25])
+enemy_health = random.choice([100, 75, 125])
 
-enemy = ['The Ember Warrior','The Aqua Warrior', 'The Sandy Warrior', 'The Frost Warrior','The Storm Warrior' ]
+enemy = ['The Ember Warrior','The Aqua Warrior', 'The Sandy Warrior','The Storm Warrior' ]
 enemy = random.choice(enemy)
 
 
 inventory = []
 player_health = 100
+player_location = rooms[0]
 turn_count = 0
 
 weapons = ["Firing Squad", "Rifle", "Grenade"]
